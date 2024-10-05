5ab7dab607c2ca6b5685e5b462a1bc0006d1a958
nidharshana-s <nidharshana0402@gmail.com>
2024-10-05 21:03:00 +0530

    leaderboard template done

--- src/components/Leaderboard.tsx
+++ src/components/Leaderboard.tsx
@@ -1,7 +1,5 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
-import * as React from "react";
-//import Image from "next/image"; // If using Next.js, or simply an <img /> tag for React
 function Leaderboard() {
   const users = [
     { rank: 1, name: "Mithun", rollNo: "22PC19", course: "MSc CS", score: 100 },
@@ -16,40 +14,32 @@ function Leaderboard() {
     <div className="bg-black text-white min-h-screen p-6">
       <div className="text-center text-pink-500 text-4xl font-bold">LEADERBOARD</div>
 
-      {/* Top 3 Cards */}
       <div className="flex justify-center mt-10 space-x-6">
-        {/* Second Place */}
         <div className="bg-gray-900 p-4 text-center rounded-lg w-40">
         <Avatar className="h-[95px] w-[95px] ml-5">
           <AvatarImage src="https://github.com/shadcn.png" className="" />
         </Avatar>
-          {/* <Image src=public\vite.svg alt="User 2" className="w-20 h-20 rounded-full mx-auto" /> */}
           <div className="text-pink-500 font-bold mt-4">Mithun</div>
           <div className="text-red-500 text-xl">100</div>
         </div>
 
-        {/* First Place */}
         <div className="bg-gray-900 p-4 text-center rounded-lg w-60">
         <Avatar className="h-[120px] w-[120px] ml-10">
           <AvatarImage src="https://github.com/shadcn.png" className="" />
         </Avatar>
-          {/* <Image src={profilePic} alt="User 1" className="w-24 h-24 rounded-full mx-auto" /> */}
           <div className="text-pink-500 font-bold mt-4">Mithun</div>
           <div className="text-purple-500 text-3xl">300</div>
         </div>
 
-        {/* Third Place */}
         <div className="bg-gray-900 p-4 text-center rounded-lg w-40">
         <Avatar className="h-[65px] w-[65px] ml-8 mt-2">
           <AvatarImage src="https://github.com/shadcn.png" className="" />
         </Avatar>
-          {/* <Image src={profilePic} alt="User 3" className="w-20 h-20 rounded-full mx-auto" /> */}
           <div className="text-pink-500 font-bold mt-4">Mithun</div>
           <div className="text-orange-500 text-xl">50</div>
         </div>
       </div>
 
-      {/* Scrollable User List */}
       <div className="mt-10 bg-gray-800 p-6 rounded-lg overflow-y-auto h-60">
         <div className="flex justify-between text-pink-500 font-semibold mb-2">
           <div>Rank</div>
